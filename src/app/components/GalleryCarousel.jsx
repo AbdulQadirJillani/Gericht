21feb4014ffdbc02aaf48b6379b5fc4012ff6453
AbdulQadirJillani <abdulqadir786687786@gmail.com>
2024-07-06 15:10:58 +0500

    carousel modified

--- src/app/components/GalleryCarousel.jsx
+++ src/app/components/GalleryCarousel.jsx
@@ -6,9 +6,6 @@ import pic2 from "@/assets/gallery02.png"
 import pic3 from "@/assets/gallery03.png"  
 import pic4 from "@/assets/gallery04.png"
 import pic5 from "@/assets/gallery05.png"  
-import { useEffect, useState } from "react"
-import { collection, getDocs } from "firebase/firestore"
-import { db } from "../firebase-config"
 
 const x = [
   {imgId: 'beiwoqd', imgLink: pic1},
@@ -19,25 +16,10 @@ const x = [
 ]
 
 const GalleryCarousel = () => {
-  const [links, setLinks] = useState()
-  useEffect(()=>{
-    async function dataFetch() {
-      try {
-          const quierySnapshot = await getDocs(collection(db, 'bbqs'))
-          const y = quierySnapshot.docs.map((d)=>({imgLink: d.data().img, imgId: d.id}))
-          setLinks([...x, ...y])
-      }
-      catch(e) {
-          console.error(e)
-      }
-    }
-    dataFetch()
-  }, [])
-
   return (
     <Carousel opts={{loop:true, align: 'start'}} plugins={[Autoplay({delay: 3000, stopOnInteraction: false})]} className="w-full max-w-2xl relative">
       <CarouselContent>
-        {links && links.map((obj)=>(
+        {x.map((obj)=>(
           <CarouselItem key={obj.imgId} className="flex items-center justify-center md:basis-1/2 lg:basis-1/3"><img className="h-[60svh]" src={obj.imgLink}/></CarouselItem>
         ))}
       </CarouselContent>
